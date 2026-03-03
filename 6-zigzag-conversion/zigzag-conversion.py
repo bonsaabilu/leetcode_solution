48680b7c7e8d0f01eb62a45f40e03ff3ea4344d7
bonsaabilu <bonsaabilu2@gmail.com>
2026-03-03 07:57:56 -0800

    Time: 10 ms (78.30%) | Memory: 12.6 MB (57.75%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -1,20 +1,19 @@
 class Solution:
-    def convert(self, s: str, numRows: int) -> str:
-        
+    def convert(self, s, numRows):
         if numRows == 1 or numRows >= len(s):
             return s
         
         rows = [""] * numRows
         current_row = 0
-        going_down = False
+        direction = -1   # will change to 1 first
         
         for char in s:
             rows[current_row] += char
             
-            # Change direction at top or bottom
+            # change direction at top or bottom
             if current_row == 0 or current_row == numRows - 1:
-                going_down = not going_down
-            
-            current_row += 1 if going_down else -1
+                direction *= -1
+                
+            current_row += direction
         
         return "".join(rows)
