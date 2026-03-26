bf329e584172d653bbfa1298b3c4e998f8c064e0
bonsaabilu <bonsaabilu2@gmail.com>
2026-03-26 12:32:31 -0800

    Time: 59 ms (52.29%) | Memory: 29.5 MB (79.08%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,27 +1,15 @@
 class Solution:
     def maxArea(self, height: list[int]) -> int:
-        # Two pointers: left starts at beginning, right at the end
-        left = 0
-        right = len(height) - 1
-        
-        max_water = 0
-        
-        # Keep moving until pointers meet
+        left, right = 0, len(height) - 1
+        max_area = 0
+
         while left < right:
-            # Area = width × min(height of two lines)
-            current_width = right - left
-            current_height = min(height[left], height[right])
-            current_area = current_width * current_height
-            
-            # Update max if current is bigger
-            if current_area > max_water:
-                max_water = current_area
-            
-            # Move the pointer with smaller height
-            # (because we want to try to find taller height to increase area)
+            width = right - left
+            max_area = max(max_area, min(height[left], height[right]) * width)
+
             if height[left] < height[right]:
                 left += 1
             else:
                 right -= 1
-        
-        return max_water
+
+        return max_area
