2a8602fd76addf74bd84e6cf4db47fb02edfff93
bonsaabilu <bonsaabilu2@gmail.com>
2026-03-26 12:33:36 -0800

    Time: 4 ms (62.35%) | Memory: 19.3 MB (50.15%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -1,30 +1,22 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        # Map of Roman numerals in descending order
-        val_to_roman = [
-            (1000, "M"),
-            (900, "CM"),
-            (500, "D"),
-            (400, "CD"),
-            (100, "C"),
-            (90, "XC"),
-            (50, "L"),
-            (40, "XL"),
-            (10, "X"),
-            (9, "IX"),
-            (5, "V"),
-            (4, "IV"),
-            (1, "I")
+        val = [
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4,
+            1
         ]
-        
-        roman = ""
-        
-        # Iterate through the values from largest to smallest
-        for value, symbol in val_to_roman:
-            if num == 0:
-                break
-            count = num // value  # How many times the symbol fits
-            roman += symbol * count
-            num -= value * count  # Reduce the number
-            
-        return roman
+        syms = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV",
+            "I"
+        ]
+        res = ""
+        i = 0
+        while num > 0:
+            count = num // val[i]
+            res += syms[i] * count
+            num -= val[i] * count
+            i += 1
+        return res
