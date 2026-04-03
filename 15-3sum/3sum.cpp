646ab01fc2a4a9b6b676c64097aced316d18f49c
bonsaabilu <bonsaabilu2@gmail.com>
2026-04-03 02:11:23 -0800

    Time: 55 ms (28.82%) | Memory: 29 MB (72.14%) - LeetSync

---
+++ 15-3sum/3sum.cpp
@@ -0,0 +1,46 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        
+        // Step 1: Sort the array
+        sort(nums.begin(), nums.end());
+        
+        // Step 2: Fix one element and use two pointers
+        for (int i = 0; i < nums.size(); i++) {
+            
+            // Skip duplicates for i
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            int left = i + 1;
+            int right = nums.size() - 1;
+            
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    
+                    // Skip duplicates for left and right
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    
+                    left++;
+                    right--;
+                }
+                else if (sum < 0) {
+                    left++;  // need bigger sum
+                }
+                else {
+                    right--; // need smaller sum
+                }
+            }
+        }
+        
+        return result;
+    }
+};
