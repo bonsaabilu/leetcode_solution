41199a2932f71f123fdb68587628400ef6115779
Bonsa Abilu <bonsaabilu2@gmail.com>
2026-05-07 10:35:46 -0800

    Time: 48 ms (43.71%) | Memory: 19.2 MB (31.22%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,12 +1,17 @@
 class Solution:
     def reverse(self, x: int) -> int:
+        # Handle sign
         sign = -1 if x < 0 else 1
         x *= sign
+        
         res = 0
         while x != 0:
             res = res * 10 + x % 10
             x //= 10
+        
         res *= sign
+        
+        # Check 32-bit signed integer range
         if res < -2**31 or res > 2**31 - 1:
             return 0
         return res
