528b4440faef7ca8a1c60ae0cd88d6ca4232c1b0
Bonsa Abilu <bonsaabilu2@gmail.com>
2026-07-16 12:36:46 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (56.96%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,12 +1,8 @@
-from typing import List
-
 class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        k = 0
-        
+    def removeElement(self, nums: list[int], val: int) -> int:
+        k = 0  # pointer for the next position to place a non-val element
         for i in range(len(nums)):
             if nums[i] != val:
                 nums[k] = nums[i]
                 k += 1
-                
         return k
