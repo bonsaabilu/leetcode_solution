2cd13d9b82876c3e733eec21115ecc76e1fe25fb
Bonsa Abilu <bonsaabilu2@gmail.com>
2026-07-23 02:23:16 -0700

    Time: 1 ms (49.41%) | Memory: 19.3 MB (79.83%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,9 +1,11 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
+        # Step 1: Trim leading whitespace
         s = s.lstrip()
         if not s:
             return 0
 
+        # Step 2: Handle sign
         sign = 1
         i = 0
         if s[0] in ['-', '+']:
@@ -11,12 +13,15 @@ class Solution:
                 sign = -1
             i += 1
 
+        # Step 3: Read digits
         res = 0
         while i < len(s) and s[i].isdigit():
             res = res * 10 + int(s[i])
             i += 1
 
         res *= sign
+
+        # Step 4: Clamp to 32-bit signed integer range
         INT_MIN, INT_MAX = -2**31, 2**31 - 1
         if res < INT_MIN:
             return INT_MIN
