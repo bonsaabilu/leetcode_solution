9b5d4b9767dfe3588f8e55d210d3ce17e6263365
Bonsa Abilu <bonsaabilu2@gmail.com>
2026-08-04 20:27:44 -0700

    Time: 0 ms (100.00%) | Memory: 19.2 MB (87.94%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,14 +1,13 @@
-from typing import List
-
 class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
+    def plusOne(self, digits: list[int]) -> list[int]:
         n = len(digits)
-        
+
+        # Traverse from the last digit backwards
         for i in range(n - 1, -1, -1):
             if digits[i] < 9:
                 digits[i] += 1
                 return digits
-            digits[i] = 0
-        
-        return [1] + digits
+            digits[i] = 0  # carry over
 
+        # If all digits were 9, we need an extra digit at the front
+        return [1] + digits
